f7772074a394b313632c20f43dc6465b28144cff
pinsoftware <31682180+pinsoftware@users.noreply.github.com>
2018-02-02 22:23:39 -0800

    update db

--- craft/config/db.php
+++ craft/config/db.php
@@ -22,6 +22,6 @@ return array(
 	'password' => 'B6bLQxHRxkSTPQwTNYKe',
 
 	// The prefix to use when naming tables. This can be no more than 5 characters.
-	'tablePrefix' => 'craft',
+	'tablePrefix' => 'wordpress',
 
 );
